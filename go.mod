bfcef6d66ba23e48912e86ea3088fcd53feb4348
SulaimonShittu <71654123+SulaimonShittu@users.noreply.github.com>
2025-03-12 14:08:07 +0100

    Exercise 2-4

--- go.mod
+++ go.mod
@@ -1,1 +1,3 @@
 module Learning-Go-Bodner-Solutions
+
+go 1.24
--- main.go
+++ main.go
@@ -1,40 +1,5 @@
 package main
 
-import (
-	"fmt"
-	"math"
-)
-
 func main() {
 
-}
-
-func exerciseTwoQ1() {
-	// Question one
-	var i int = 20
-	var f float64 = float64(i)
-	fmt.Println(i, f)
-}
-
-func exerciseTwoQ2() {
-	// Question two
-	const value = 12
-	var i int = value
-	var f float64 = value
-	fmt.Println(i, f)
-}
-
-func exerciseTwoQ3() {
-	// Question Three
-	var (
-		b      byte   = math.MaxUint8
-		smallI int32  = math.MaxInt32
-		bigI   uint64 = math.MaxUint64
-	)
-
-	b += 1
-	smallI += 1
-	bigI += 1
-
-	fmt.Println(b, smallI, bigI)
 }
