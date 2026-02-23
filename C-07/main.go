f222dc71f88837e3baefaed31abd8c2a323bffd5
sulaimon <sulele04@gmail.com>
2026-02-23 18:47:45 +0100

    - chap 7 done

--- C-07/main.go
+++ C-07/main.go
@@ -2,6 +2,9 @@ package main
 
 import (
 	"fmt"
+	"io"
+	"maps"
+	"os"
 )
 
 type TeamName string
@@ -18,6 +21,9 @@ type League struct {
 
 func (l *League) MatchResult(firstTeam TeamName,
 	firstTeamScore int, secondTeam TeamName, secondTeamScore int) {
+	if l.Wins == nil {
+		l.Wins = map[TeamName]int{}
+	}
 	if firstTeamScore > secondTeamScore {
 		l.Wins[firstTeam]++
 		return
@@ -30,22 +36,76 @@ func (l *League) MatchResult(firstTeam TeamName,
 
 func (l *League) Ranking() []TeamName {
 	teamRanking := make([]TeamName, 0, len(l.Teams))
-	type teamm struct {
-		name TeamName
-		wins int
+	data := maps.Clone(l.Wins)
+	for len(data) > 0 {
+		var maxWin int
+		var teamName TeamName
+		for k, v := range data {
+			if v > maxWin {
+				maxWin = v
+				teamName = k
+			}
+		}
+		teamRanking = append(teamRanking, teamName)
+		delete(data, teamName)
 	}
-	data := make([]teamm, 0, len(l.Wins))
-	for k, v := range l.Wins {
-		data = append(data, teamm{
-			k,
-			v,
-		})
-	}
-	sortedTeams := make([]teamm, 0, len(data))
-	fmt.Println(sortedTeams)
 	return teamRanking
 }
 
-func main() {
+type Ranker interface {
+	Ranking() []TeamName
+}
 
+func RankPrinter(r Ranker, writer io.Writer) {
+	for _, v := range r.Ranking() {
+		io.WriteString(writer, fmt.Sprintln(string(v)))
+	}
+}
+
+func main() {
+	// E-02
+	epl := &League{
+		Teams: []Team{
+			{
+				Name: "Chelsea",
+				PlayersNames: []string{
+					"Delap", "Palmer", "Joao Pedro", "Caicedo", "James",
+				},
+			},
+			{
+				Name: "Arsenal",
+				PlayersNames: []string{
+					"Saka", "Madueke", "Eze", "Gyokeres", "Saliba",
+				},
+			},
+			{
+				Name: "Man City",
+				PlayersNames: []string{
+					"Cherki", "Marmoush", "Haaland", "Rodri", "Semenyo",
+				},
+			},
+			{
+				Name: "Spurs",
+				PlayersNames: []string{
+					"Gray", "Spence", "Simons", "Odobert", "Udogie",
+				},
+			},
+			{
+				Name: "Nottingham Forrest",
+				PlayersNames: []string{
+					"Hudson-Odoi", "Gibbs White", "Aina", "Woods", "Ndoye",
+				},
+			},
+		},
+	}
+	epl.MatchResult("Chelsea", 3, "Spurs", 0)
+	epl.MatchResult("Chelsea", 4, "Arsenal", 2)
+	epl.MatchResult("Chelsea", 4, "Man City", 2)
+	epl.MatchResult("Chelsea", 2, "Nottingham Forrest", 0)
+	epl.MatchResult("Nottingham Forrest", 3, "Spurs", 0)
+	epl.MatchResult("Arsenal", 4, "Man City", 5)
+	epl.MatchResult("Spurs", 0, "Man City", 5)
+	epl.MatchResult("Arsenal", 2, "Nottingham Forrest", 0)
+
+	RankPrinter(epl, os.Stdout)
 }
